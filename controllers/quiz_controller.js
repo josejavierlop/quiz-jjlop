83b910a7e3208650c83ded74ec06d2be1fdd2b66
josejavierlop <josejavierlop@gmail.com>
2015-07-08 16:15:06 +0200

    Despliegue DB Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,24 @@
+var models = require('../models/models.js');
 //GET /quizes/question
 exports.question = function(req, res){
-	res.render('quizes/question', {pregunta : 'Capital de Italia'});
+	models.Quiz.findAll().success(
+		function(quizes){
+			res.render('quizes/question', {pregunta : quizes[0].pregunta});
+		}
+	);
+	
 };
 
 //GET /quizes/answer
 exports.answer = function(req, res){
-	if(req.query.respuesta === 'Roma'){
-		res.render('quizes/answer', {respuesta : 'Correcto'});
-	}else{
-		res.render('quizes/answer', {respuesta : 'Incorrecto'});
-	}
+	models.Quiz.findAll().success(
+		function(quizes){
+			if(req.query.respuesta === quizes[0].respuesta){
+				res.render('quizes/answer', {respuesta : 'Correcto'});
+			}else{
+				res.render('quizes/answer', {respuesta : 'Incorrecto'});
+			}
+		}
+	);
+	
 };
